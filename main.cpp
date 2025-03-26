0a792eb19861ffed4cd2d68616a5aa81cb6be2c8
SARAH DAVIS <sdavis2@nic.edu>
2025-03-26 10:39:07 -0700

    add function call

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void say(string s);
+
+int add(int a, int b);
+
 int main()
 {
     cout << "Greeting....\n";
     cout << "Hello World\n";
-
+    say("hi");
     return 0;
+}
+
+void say(string s)
+{
+    cout << s << endl;
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
