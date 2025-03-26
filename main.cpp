27533136bb7b4c95edbf1b879f69bc70b668508c
sarahdavis2008 <sdavis2@nic.edu>
2025-03-26 17:24:41 +0000

    created add

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int add(int a, int b);
+
 int main()
 {
     cout << "Hello World\n";
 
     return 0;
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
